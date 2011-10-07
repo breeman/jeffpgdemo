88f7c85710ec3833a7d13fa8349d6cf155eaa0e6
breeman <jnixon@eduventures.com>
2011-10-07 13:51:00 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jeffpgdemo::Application.config.secret_token = '0ad0f4ce8db582c48cd7407d26fd0be8c3d6d97d04538f5660dfbb116f6d277ed439a928c5d23314ed7a7b89531b9645e942038b86d1b0f23c083defb332a646'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Jeffpgdemo::Application.config.session_store :cookie_store, key: '_jeffpgdemo_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Jeffpgdemo::Application.config.session_store :active_record_store
